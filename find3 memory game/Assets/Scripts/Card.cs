bb8f7483722d9727cd47cb8993e8244bf7878ce6
milan_bogosavljevic <andrej792885@gmail.com>
2020-05-31 22:32:49 +0200

    files added

---
+++ find3 memory game/Assets/Scripts/Card.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private float _yRotation;
+    private float _flipAnimationSpeed;
+    private bool _cardIsFliped;
+    private bool _cardIsClicked;
+    private bool _playAnimation;
+    private bool _scaleCard;
+    [SerializeField] private Sprite cardBack;
+    [SerializeField] private Sprite cardFront;
+    [SerializeField] private GameController gameController;
+    private SpriteRenderer _cardImage;
+
+    public Card refference;
+    void Start()
+    {
+        _cardIsFliped = false;
+        _cardIsClicked = false;
+        _playAnimation = false;
+        _scaleCard = false;
+        _cardImage = gameObject.GetComponent<SpriteRenderer>();
+        _yRotation = 0;
+        _flipAnimationSpeed = 3f;
+
+        refference = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playAnimation)
+        {
+            _flipCard();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!_cardIsClicked)
+        {
+            _cardIsClicked = true;
+            _startAnimation();
+            //gameController.CardIsSelected(refference);
+            gameController.CardIsSelected(gameObject);
+            //StartCoroutine(ExampleCoroutine());
+        }
+    }
+
+    private void _startAnimation()
+    {
+        _scaleCard = true;
+        _playAnimation = true;
+    }
+    
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        _startAnimation();
+    }
+
+    private void _flipCard()
+    {
+        _yRotation = _scaleCard ? _yRotation + _flipAnimationSpeed : _yRotation - _flipAnimationSpeed;
+        if (_yRotation > 90f)
+        {
+            _yRotation = 90f;
+            _cardImage.sprite = _cardIsFliped ? cardBack : cardFront;
+            _scaleCard = false;
+        }
+
+        if (_yRotation < 0f)
+        {
+            _yRotation = 0f;
+            _scaleCard = true;
+            _playAnimation = false;
+            _cardIsFliped = true;
+        }
+        transform.rotation = Quaternion.Euler(0, _yRotation, 0);
+    }
+}
---
+++ find3 memory game/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private List<Card> allCards;
+    private List<GameObject> _selectedCards;
+
+    //public void CardIsSelected(Card selectedCard)
+    public void CardIsSelected(GameObject selectedCard)
+    {
+        Debug.Log(selectedCard.GetComponent<Card>());
+        //_selectedCards.Add(selectedCard);
+        //Debug.Log(_selectedCards);
+    }
+}
