1df02529b5cb57dc05bf9931e0296945b9ed5345
milan_bogosavljevic <andrej792885@gmail.com>
2020-07-03 23:06:28 +0200

    level selection and welcome scenes added

---
+++ find3 memory game/Assets/Scripts/LevelSelectionButtonsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectionButtonsController : MonoBehaviour
+{
+    [SerializeField] private List<Button> levelButtons;
+
+    void Start()
+    {
+        _setButtonsBestTime();
+    }
+
+    private void _setButtonsBestTime()
+    {
+        for (int i = 0; i < levelButtons.Count; i++)
+        {
+            int levelNumber = i + 1;
+            string minutesKey = "level" + levelNumber + "Minutes";
+            string secondsKey = "level" + levelNumber + "Seconds";
+            string time = "00:00";
+            if (PlayerPrefs.HasKey(minutesKey))
+            {
+                int minutes = PlayerPrefs.GetInt(minutesKey);
+                int seconds = PlayerPrefs.GetInt(secondsKey);
+                string minutesText = minutes < 10 ? "0" + minutes.ToString() : minutes.ToString();
+                string secondsText = seconds < 10 ? "0" + seconds.ToString() : seconds.ToString();
+                time = minutesText + ":" + secondsText;
+                print(levelNumber);
+                print(time);
+            }
+            levelButtons[i].GetComponentInChildren<Text>().text = time;
+        }
+    }
+    
+    public void LoadLevel(int levelNum)
+    {
+        int levelIndex = levelNum + 1;
+        SceneManager.LoadScene(levelIndex);
+    }
+}
